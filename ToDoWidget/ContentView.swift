ad9c46ce667a48295549599478b2d3651ec17480
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-10-25 17:54:51 -0500

    Initial Commit

---
+++ ToDoWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoWidget
+//
+//  Created by Renato Oliveira Fraga on 10/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoWidget/ToDoWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoWidgetApp.swift
+//  ToDoWidget
+//
+//  Created by Renato Oliveira Fraga on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
